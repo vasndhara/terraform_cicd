f1471bd2b9feef39ecec00f05db7a491db8835a5
vasndhara <167391804+vasndhara@users.noreply.github.com>
2024-07-23 23:20:21 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "vpc_id" { 
+ value = aws_vpc.myvpc.id
+ 
+} 
+output "public_subnet_id" { 
+ value = aws_subnet.PublicSubnet.id
+} 
+output "private_subnet_id" { 
+ value = aws_subnet.PrivSubnet.id
+} 
+output "route_table_id" { 
+ value = aws_route_table.PublicRT.i
